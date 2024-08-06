cc93d8892c509d01e074323ca7804f381d71e494
Joshtri <stuffofyos1516@gmail.com>
2024-08-06 21:30:11 +0800

    fix controller and model

--- src/models/presensi.model.js
+++ src/models/presensi.model.js
@@ -1,40 +1,50 @@
 import mongoose from "mongoose";
 
 const presensiSchema = new mongoose.Schema({
-
-    pesertaId:{
+    pesertaId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:'Peserta',
+        ref: 'Peserta',
         required: true
     },
-
-    jam_masuk:{
-        type:Date,
-        required:true
+    jam_masuk: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^([01]\d|2[0-3]):([0-5]\d)$/.test(v); // 24-hour format HH:mm
+            },
+            message: props => `${props.value} is not a valid time!`
+        }
     },
-
-    jam_keluar:{
-        type:Date,
-        required:true
+    jam_keluar: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^([01]\d|2[0-3]):([0-5]\d)$/.test(v); // 24-hour format HH:mm
+            },
+            message: props => `${props.value} is not a valid time!`
+        }
     },
-    live_sesi:{
-        type:Number,
-        required:false
+    live_sesi: {
+        type: Number,
+        required: false
     },
-    status_kehadiran:{
-        type:String,
-        enum:[
+    status_kehadiran: {
+        type: String,
+        enum: [
             'hadir',
             'alpha',
             'izin',
             'sakit'
         ],
-        required:true,
-        default:'alpha'
+        required: true,
+        default: 'alpha'
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
-const Presensi = mongoose.model("Presensi", presensiSchema);
+const Presensi = mongoose.model('Presensi', presensiSchema);
+
 export default Presensi;
