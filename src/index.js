0c4c240bc16693f020877d0c5974d81b17fd3c7a
Joshtri <stuffofyos1516@gmail.com>
2024-08-06 12:53:06 +0800

    lts

--- src/index.js
+++ src/index.js
@@ -19,10 +19,10 @@ const PORT = process.env.APP_PORT;
 // app.use(bodyParser.json());
 // app.use(express.urlencoded(true));
 
-if(process.env.APP_ENABLE_CORS === 'true'){
+if(process.env.APP_ENABLE_CORS === true){
     app.use(cors({
         origin: process.env.APP_CORS_ORIGINS.split(','),
-        // methods: [POST, GET],
+        methods: [POST, GET],
         credentials:true
     }));
 }
