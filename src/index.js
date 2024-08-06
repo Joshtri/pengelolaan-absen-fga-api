ae1365b3726beba8060900e348080d6619551d34
Joshtri <stuffofyos1516@gmail.com>
2024-08-06 11:37:50 +0800

    cors config updt

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 import connectDB from './config/dbConfig.js';
 import pesertaRoute from './routes/peserta.route.js';
 import presensiRoute from './routes/presensi.route.js';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 
 config();
@@ -16,13 +16,16 @@ const PORT = process.env.APP_PORT;
 
 
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 // app.use(express.urlencoded(true));
-app.use(cors({
-    origin: '*',
-    // methods: [POST, GET],
-    credentials:true
-}));
+
+if(process.env.APP_ENABLE_CORS === 'true'){
+    app.use(cors({
+        origin: process.env.APP_CORS_ORIGINS.split(','),
+        // methods: [POST, GET],
+        credentials:true
+    }));
+}
 app.use(express.json());
 
 
